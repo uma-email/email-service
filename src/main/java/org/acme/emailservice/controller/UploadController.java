6822cf9114c48801135b4e9455628b4dc106b071
1token <zboran@federizer.com>
2021-05-05 09:08:11 +0200

    upload resources

--- src/main/java/org/acme/emailservice/controller/UploadController.java
+++ src/main/java/org/acme/emailservice/controller/UploadController.java
@@ -1,15 +1,15 @@
 package org.acme.emailservice.controller;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
-import javax.inject.Inject;
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -17,56 +17,66 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-import org.acme.emailservice.service.MultipartService;
-import org.apache.commons.io.IOUtils;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
-import org.wildfly.common.format.Printf;
 
-@Path("/client")
+@Path("/rs")
+@RolesAllowed({ "user", "admin" })
 public class UploadController {
 
     // @Inject
     // @RestClient
     // MultipartService service;
 
-    private final String UPLOADED_FILE_PATH = ""; // will go to 'target' folder
+    public class ResourceFile {
+
+        public List<String> resourceName;
+
+        public ResourceFile() {
+            this.resourceName = new ArrayList<>();
+        }
+
+        public void addFilename(String resourceName) {
+            this.resourceName.add(resourceName);
+        }
+    }
+
+    private final String UPLOADED_FILE_PATH = ""; // an message attachment draft folder
 
     @POST
     @Path("/upload")
-    @Consumes({MediaType.MULTIPART_FORM_DATA})
+    @Consumes({ MediaType.MULTIPART_FORM_DATA })
+    @Produces(MediaType.APPLICATION_JSON)
     public Response uploadFile(MultipartFormDataInput input) throws IOException {
 
-        String fileName = "";
+        ResourceFile resourceFile = new ResourceFile();
 
         Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
         List<InputPart> inputParts = uploadForm.get("uploadedFile");
         List<InputPart> inputParts2 = uploadForm.get("messageId");
 
         for (InputPart inputPart2 : inputParts2) {
-            System.out.printf(inputPart2.getBodyAsString());
+            System.out.printf(inputPart2.getBodyAsString() + "\n");
         }
 
         for (InputPart inputPart : inputParts) {
 
             try {
+                String resourceName = UUID.randomUUID().toString();
+                resourceFile.addFilename(resourceName);
 
                 MultivaluedMap<String, String> header = inputPart.getHeaders();
-                fileName = getFileName(header);
+                String fileName = getFileName(header);
 
                 // convert the uploaded file to inputstream
                 InputStream inputStream = inputPart.getBody(InputStream.class, null);
 
-                byte[] bytes = IOUtils.toByteArray(inputStream);
-
                 // constructs upload file path
-                fileName = UPLOADED_FILE_PATH + fileName;
+                resourceName = UPLOADED_FILE_PATH + resourceName;
 
-                writeFile(bytes, fileName);
-
-                System.out.println("Done");
+                writeToResourceFile(inputStream, resourceName);
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -74,14 +84,9 @@ public class UploadController {
 
         }
 
-        return Response.status(200).entity("uploadFile is called, Uploaded file name : " + fileName).build();
+        return Response.status(Status.CREATED).entity(resourceFile).build();
     }
 
-    /**
-     * header sample { Content-Type=[image/png], Content-Disposition=[form-data;
-     * name="file"; filename="filename.extension"] }
-     **/
-    // get uploaded filename, is there a easy way in RESTEasy?
     private String getFileName(MultivaluedMap<String, String> header) {
 
         String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
@@ -98,20 +103,27 @@ public class UploadController {
         return "unknown";
     }
 
-    //save to somewhere
-    private void writeFile(byte[] content, String filename) throws IOException {
+    private void writeToResourceFile(InputStream content, String resourceName) {
 
-        File file = new File(filename);
+        int read = 0;
+        byte[] bytes = new byte[1024];
 
-        if (!file.exists()) {
-            file.createNewFile();
+        try {
+            File file = new File(resourceName);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileOutputStream out = new FileOutputStream(file);
+            while ((read = content.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        FileOutputStream fop = new FileOutputStream(file);
-
-        fop.write(content);
-        fop.flush();
-        fop.close();
-
     }
 }
--- src/main/java/org/acme/emailservice/rest/MessageApi.java
+++ src/main/java/org/acme/emailservice/rest/MessageApi.java
@@ -9,12 +9,13 @@ import java.util.Map;
 import java.util.UUID;
 
 import javax.annotation.security.RolesAllowed;
-import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
@@ -22,18 +23,28 @@ import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 // Resource server path
 @Path("/rs")
-@RequestScoped
 @RolesAllowed({ "user", "admin" })
 public class MessageApi {
 
-    private final String MESSAGES_FILE_PATH = ""; // a message target folder
+    private final String MESSAGES_FILE_PATH = ""; // a message draft folder
+
+    public class ResourceFile {
+
+        public String resourceName;
+
+        public ResourceFile(String resourceName) {
+            this.resourceName = resourceName;
+        }
+    }
 
     @POST
     @Path("/message")
     @Consumes({ MediaType.MULTIPART_FORM_DATA })
+    @Produces(MediaType.APPLICATION_JSON)
     public Response uploadFile(MultipartFormDataInput input) throws IOException {
 
-        String fileName = UUID.randomUUID().toString();
+        String resourceName = UUID.randomUUID().toString();
+        ResourceFile resourceFile = new ResourceFile(resourceName);
 
         Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
         List<InputPart> messageTextParts = uploadForm.get("message-text");
@@ -48,28 +59,34 @@ public class MessageApi {
                 InputStream inputStream = messageTextParts.get(0).getBody(InputStream.class, null);
                 byte[] bytes = IOUtils.toByteArray(inputStream);
 
-                fileName = MESSAGES_FILE_PATH + fileName;
-                writeFile(bytes, fileName);
+                resourceName = MESSAGES_FILE_PATH + resourceName;
+                writeToResourceFile(bytes, resourceName);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        return Response.status(200).entity("uploadFile is called, Uploaded file name : " + fileName).build();
+        return Response.status(Status.CREATED).entity(resourceFile).build();
     }
 
-    private void writeFile(byte[] content, String filename) throws IOException {
-        File file = new File(filename);
+    private void writeToResourceFile(byte[] content, String resourceName) throws IOException {
 
-        if (!file.exists()) {
-            file.createNewFile();
+        try {
+            File file = new File(resourceName);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileOutputStream out = new FileOutputStream(file);
+
+            out.write(content);
+            out.flush();
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        FileOutputStream fop = new FileOutputStream(file);
-
-        fop.write(content);
-        fop.flush();
-        fop.close();
     }
 
 }
