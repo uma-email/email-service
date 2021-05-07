3b0ac54d0e8d75ac1acf061b22dc5267f6ef710e
1token <zboran@federizer.com>
2021-05-07 14:49:30 +0200

    message update

--- src/main/java/org/acme/emailservice/rest/MessageApi.java
+++ src/main/java/org/acme/emailservice/rest/MessageApi.java
@@ -12,11 +12,14 @@ import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -29,6 +32,7 @@ import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 @RolesAllowed({ "user", "admin" })
 public class MessageApi {
 
+    protected @Context Request request;
     private final String MESSAGES_FILE_PATH = ""; // a message draft folder
 
     public class ResourceFile {
@@ -39,36 +43,58 @@ public class MessageApi {
         }
     }
 
-    @POST
+    @POST @PUT
     @Path("/message")
     @Consumes({ MediaType.MULTIPART_FORM_DATA })
     @Produces(MediaType.APPLICATION_JSON)
     public Response uploadFile(MultipartFormDataInput input) throws IOException {
 
-        String resourceName = UUID.randomUUID().toString();
-        ResourceFile resourceFile = new ResourceFile(resourceName);
+        String resourceName = null;
+        
+        Status status = Status.BAD_REQUEST;
 
         Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
-        List<InputPart> messageTextParts = uploadForm.get("message-text");
-        List<InputPart> messageTextIdParts = uploadForm.get("text-message-id");
+        List<InputPart> messageParts = uploadForm.get("message-text");
+        List<InputPart> messageIdParts = uploadForm.get("text-message-id");
+        List<InputPart> messageResourceNameParts = uploadForm.get("text-message-resource-name");
 
-        String message = messageTextParts.get(0).getBodyAsString();
-        String messageTextId = messageTextIdParts.get(0).getBodyAsString();
-        long messageId = messageTextId == null ? 0 : Long.parseLong(messageTextId);
+        String message = messageParts.size() == 0 ? null : messageParts.get(0).getBodyAsString();
+        String messageTextId = messageIdParts.size() == 0 ? null : messageIdParts.get(0).getBodyAsString();
+        String messageResourceName = messageResourceNameParts.size() == 0 ? null : messageResourceNameParts.get(0).getBodyAsString();
 
-        if (message != null && messageId > 0) {
+        if (request.getMethod().contains("PUT")) {
+            resourceName = messageResourceName;
+        } else {
+            resourceName = UUID.randomUUID().toString();
+        }
+
+        ResourceFile resourceFile = new ResourceFile(resourceName);
+
+        if (resourceName != null && resourceName.length() > 0) {
             try {
-                InputStream inputStream = messageTextParts.get(0).getBody(InputStream.class, null);
+                InputStream inputStream = messageParts.get(0).getBody(InputStream.class, null);
                 byte[] bytes = IOUtils.toByteArray(inputStream);
 
                 resourceName = MESSAGES_FILE_PATH + resourceName;
                 writeToResourceFile(bytes, resourceName);
+
+                long messageId = messageTextId == null ? 0 : parseToInt(messageTextId, 0);
+
+                if (message != null && messageId > 0) {
+                    // write to database
+                }                
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        return Response.status(Status.CREATED).entity(resourceFile).build();
+        if (request.getMethod().contains("PUT")) {
+            status = Status.OK;
+        } else {
+            status = Status.CREATED;
+        }
+
+        return Response.status(status).entity(resourceFile).build();
     }
 
     @GET
@@ -80,6 +106,14 @@ public class MessageApi {
 		File fileDownload = new File(MESSAGES_FILE_PATH + resourceName);
         String fileName = "text.txt";
 		return Response.ok((Object) fileDownload).header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + fileName).build();
+    }
+
+    private static int parseToInt(String stringToParse, int defaultValue) {
+        try {
+           return Integer.parseInt(stringToParse);
+        } catch(NumberFormatException ex) {
+           return defaultValue; //Use default value if parsing failed
+        }
     }
 
     private void writeToResourceFile(byte[] content, String resourceName) throws IOException {
